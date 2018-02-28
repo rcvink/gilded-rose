7a30a584ef4b007d0d8468e2af9c8531679bbdc5
rcvink <rrvink@gmail.com>
2018-02-28 16:35:15 +0000

    Refactor tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,12 +2,12 @@ require 'updater_factory'
 
 class GildedRose
 
-  def initialize(items)
+  def initialize(items, updater_factory = UpdaterFactory.new)
     @items = items
-    @updater_factory = UpdaterFactory.new
+    @updater_factory = updater_factory
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
       @updater_factory.create(item).update
     end
