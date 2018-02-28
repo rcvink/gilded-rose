8fbc55f063761d56a90ea9f095e22c48d780deaf
rcvink <rrvink@gmail.com>
2018-02-28 14:06:44 +0000

    Add regular updater

--- lib/regular_updater.rb
+++ lib/regular_updater.rb
@@ -1,3 +1,36 @@
-
 class RegularUpdater
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    unless min_quality?
+      if past_sell_by?
+        decrement_quality_by(2)
+      else
+        decrement_quality_by(1)
+      end
+    end
+    decrement_sell_in
+  end
+
+  private
+
+  def min_quality?
+    return @item.quality <= 0
+  end
+
+  def past_sell_by?
+    return @item.sell_in <= 0
+  end
+
+  def decrement_quality_by(amount)
+    @item.quality -= amount
+  end
+
+  def decrement_sell_in
+    @item.sell_in -= 1
+  end
+
 end
--- spec/regular_updater_spec.rb
+++ spec/regular_updater_spec.rb
@@ -1,9 +1,64 @@
-require 'rspec'
+require_relative '../lib/regular_updater'
+require_relative '../lib/item'
 
 describe RegularUpdater do
 
-  it 'should ' do
+  context 'on an item with quality > 0,' do
 
-    true.should == false
+    context 'before sell by date, changes' do
+
+      before(:each) do
+        @item = Item.new('regular', 5, 10)
+        regular_updater = RegularUpdater.new(@item)
+        regular_updater.update
+      end
+
+      it 'quality by -1' do
+        expect(@item.quality).to eq 9
+      end
+
+      it 'sell_in by -1' do
+        expect(@item.sell_in).to eq 4
+      end
+
+    end
+
+    context 'after sell by date, changes' do
+
+      before(:each) do
+        @item = Item.new('regular', 0, 20)
+        regular_updater = RegularUpdater.new(@item)
+        regular_updater.update
+      end
+
+      it 'quality by -2' do
+        expect(@item.quality).to eq 18
+      end
+
+      it 'sell_in by -1' do
+        expect(@item.sell_in).to eq -1
+      end
+
+    end
+
   end
+
+  context 'on an item with quality == 0,' do
+
+    before(:each) do
+      @item = Item.new('regular', 5, 0)
+      regular_updater = RegularUpdater.new(@item)
+      regular_updater.update
+    end
+
+    it 'does not change quality' do
+      expect(@item.quality).to eq 0
+    end
+
+    it 'changes sell_in by -1' do
+      expect(@item.sell_in).to eq 4
+    end
+
+  end
+
 end
