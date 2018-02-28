6e245302212eda3343a5d90c3f332afd1f81dbc2
rcvink <rrvink@gmail.com>
2018-02-28 13:23:08 +0000

    Add BrieUpdater and tests

--- lib/brie_updater.rb
+++ lib/brie_updater.rb
@@ -1,3 +1,36 @@
-
 class BrieUpdater
+
+  def initialize(brie)
+    @brie = brie
+  end
+
+  def update
+    unless max_quality?
+      if past_sell_by?
+        increment_quality_by(2)
+      else
+        increment_quality_by(1)
+      end
+    end
+    decrement_sell_in
+  end
+
+  private
+
+  def max_quality?
+    return @brie.quality >= 50
+  end
+
+  def past_sell_by?
+    return @brie.sell_in <= 0
+  end
+
+  def decrement_sell_in
+    @brie.sell_in -= 1
+  end
+
+  def increment_quality_by(amount)
+    @brie.quality += amount
+  end
+
 end
--- spec/brie_updater_spec.rb
+++ spec/brie_updater_spec.rb
@@ -1,9 +1,64 @@
-require 'rspec'
+require_relative '../lib/gilded_rose'
+require_relative '../lib/brie_updater'
 
 describe BrieUpdater do
 
-  it 'should do something' do
+  context 'on a brie with quality < 50' do
 
-    true.should == false
+    context 'before sell by date, changes' do
+
+      before(:each) do
+        @brie = Item.new('Aged Brie', 2, 0)
+        brie_updater = BrieUpdater.new(@brie)
+        brie_updater.update
+      end
+
+      it 'quality by +1' do
+        expect(@brie.quality).to eq 1
+      end
+
+      it 'sell_in by -1' do
+        expect(@brie.sell_in).to eq 1
+      end
+
+    end
+
+    context 'after sell by date, changes' do
+
+      before(:each) do
+        @brie = Item.new('Aged Brie', 0, 0)
+        brie_updater = BrieUpdater.new(@brie)
+        brie_updater.update
+      end
+
+      it 'quality by +2' do
+        expect(@brie.quality).to eq 2
+      end
+
+      it 'sell_in by -1' do
+        expect(@brie.sell_in).to eq -1
+      end
+
+    end
+
   end
+
+  context 'on a brie with quality >= 50' do
+
+    before(:each) do
+      @brie = Item.new('Aged Brie', 3, 50)
+      brie_updater = BrieUpdater.new(@brie)
+      brie_updater.update
+    end
+
+    it 'does not change quality' do
+      expect(@brie.quality).to eq 50
+    end
+
+    it 'changes sell_in by -1' do
+      expect(@brie.sell_in).to eq 2
+    end
+
+  end
+
 end
